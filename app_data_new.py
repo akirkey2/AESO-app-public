d40993c7424a67c5fee0b0d9dd5044dc806d4943
akirkey2 <112200926+akirkey2@users.noreply.github.com>
2025-01-17 17:04:56 -0500

    First step in setting up my public AESO dash app

---
+++ app_data_new.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 13:52:32 2024
+
+@author: Aaron Kirkey
+"""
+
+
+#%% Environment setup
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from datetime import datetime
+# import seaborn as sns
+# import scipy as sp
+# import glob
+# import sys
+# from matplotlib import cm
+# import pathlib
+# import os
+# from sklearn import preprocessing
+# import plotly.io as pio
+# import plotly.express as px
+# from dash import Dash, html, dash_table, dcc, callback, Input, Output
+# import dash
+# pio.renderers.default='browser'
+mpl.rcParams.update(mpl.rcParamsDefault)
+plt.rc('axes', axisbelow=True)
+mpl.interactive(True)
+mpl.rcParams['lines.markersize'] = 5
+
+
+#%% Dataframe setup
+# file_path = 'C:\\Users\\Aaron Kirkey\\Documents\\GitHub\\AESO-data\\CSD Generation (Hourly) - 2024-01.csv'
+file_name = 'CSD Generation (Hourly) - 2024-01 to 2024-06.csv'
+df = pd.read_csv(file_name, usecols=['Date (MST)', 'Asset Name', 'Volume',
+       'Fuel Type', 'Sub Fuel Type'])
+
+
+#%% Reducing the df to the date(s) selected by the user in the dash
+date = '2024-01-06'
+
+
+def df_date_restrict(date='2024-01-01'):
+    
+    dft = df.loc[(df['Date (MST)'].str.contains(date))]
+    dfg = dft.groupby(['Date (MST)','Fuel Type'])['Volume'].sum()
+    dfg = dfg.reset_index()
+    dfg = dfg.sort_values(by=['Date (MST)','Fuel Type'])
+    # dftotal = df_date_range.groupby(['Date (MST)']).sum()
+    # dftotal.index = pd.to_datetime(dftotal.index)
+    return dfg
+
+#%%
+dft = df.loc[(df['Date (MST)'].str.contains(date))]
+dfg = dft.groupby(['Date (MST)','Fuel Type'])['Volume'].sum()
+dfg = dfg.reset_index()
+dfg = dfg.sort_values(by=['Fuel Type','Date (MST)'])
+
+#%%
+def df_hourly(date='2024-01-05'):
+    dft = df_date_restrict(date) #Get the smaller df based on date input by user
+    df_temp_2 = pd.DataFrame()
+    
+    for i in df['Fuel Type'].unique():
+        df_temp = dft.loc[(dft['Fuel Type'].str.contains(i))] 
+        df_temp = df_temp.groupby(['Date (MST)'])[['Date (MST)','Volume']].sum()
+        df_temp_2[i] = df_temp["Volume"]
+        df_temp.index = pd.to_datetime(df_temp.index)
+    df_temp_2 = df_temp_2.reset_index()
+    df_temp_2 = df_temp_2.round(decimals = 0)
+    df_temp_2['TOTAL'] = df_temp_2[['OTHER', 'WIND', 'GAS', 'HYDRO', 'SOLAR',
+           'ENERGY STORAGE', 'COAL', 'DUAL FUEL']].sum(axis=1)
+    df_temp_2 = df_temp_2[['Date (MST)','COAL','GAS','DUAL FUEL','HYDRO',
+                           'SOLAR','WIND','ENERGY STORAGE','OTHER','TOTAL']]
+    df_temp_2['NET'] = df_temp_2['TOTAL'] - (df_temp_2[['SOLAR','WIND','HYDRO']].sum(axis=1))
+    df_temp_2['FF'] = df_temp_2[['OTHER', 'GAS','COAL', 'DUAL FUEL']].sum(axis=1)
+    df_temp_2['RE'] = df_temp_2[['SOLAR','WIND','HYDRO']].sum(axis=1)
+    return df_temp_2
+    
+#%% Make the columns that represent the percent contribution to total!!!
+def df_percent(date='2024-01-05'): 
+    
+    dft = df_hourly(date) #Get the hourly volume dataframe
+    
+    df1p = pd.DataFrame()
+    df1p['Date (MST)'] = dft['Date (MST)']
+    
+    for i in dft.columns[1:9]:
+        df1p[f'{i}'] = (dft[f'{i}'] / dft['TOTAL'])*100
+    df1p['TOTAL'] = df1p[['OTHER', 'WIND', 'GAS', 'HYDRO', 'SOLAR',
+           'ENERGY STORAGE', 'COAL', 'DUAL FUEL']].sum(axis=1)
+    df1p['RENEWABLE'] = df1p[['WIND','HYDRO','SOLAR','ENERGY STORAGE']].sum(axis=1)
+    df1p['FOSSIL FUEL'] = df1p[['COAL','GAS','DUAL FUEL']].sum(axis=1)
+    df1p = df1p.round(decimals = 1)
+    return df1p
+#%%
+date = '2024-01-07'
+start_date = pd.to_datetime(date).date() #Format: %Y/%m/%d
+end_date = pd.to_datetime(date).date() + pd.Timedelta(days=7)
+
+#%%
+def price_ei_vol_day(date='2024-01-06'):
+    start_time = datetime.now()
+    dft = df.copy()
+    # dft = dft[['Date (MST)', 'Asset Short Name', 'Volume',
+    #    'Fuel Type', 'Sub Fuel Type']]
+    dft['Date (MST)'] = pd.to_datetime(df['Date (MST)'],format='%Y-%m-%d %H:%M:%S')
+    
+    
+    # Restricting df to desired dates, in app this value comes from the callback
+
+    start_date = pd.to_datetime(date).date() #Format: %Y/%m/%d
+    end_date = pd.to_datetime(date).date() #+ pd.Timedelta(days=7) #Same format
+
+    # You have to use dt.date for series and .date() for individual strings 
+    dft = dft.loc[(dft['Date (MST)'].dt.date >= start_date) & (dft['Date (MST)'].dt.date<= end_date)]
+    dfg = dft.groupby(['Date (MST)','Fuel Type'])['Volume'].sum()
+    dfg = dfg.reset_index()
+    dfg = dfg.sort_values(by=['Date (MST)','Fuel Type'])
+
+#Emission factor calculations from: https://www.nrel.gov/docs/fy21osti/80580.pdf
+# Values in gCO2e/kWh, conversion explained in next cell.
+
+#This method is used in favour of the cell above because it's more succinct and faster computationally
+
+    """"Volumes from AESO are in MW and emission factors (above) are in 
+    gCO2e/kWh. This means we have to multiply values by 1x10^3 but divide by 1x10^6
+    to get tonsCO2e. This means dividing all values by 1x10^3"""
+
+    dft['Emissions'] = np.nan
+
+    emission_factors={'Wood/Refuse':52,'WIND':13, 'SIMPLE_CYCLE':486,'COGENERATION':486,
+                  'HYDRO':24,'SOLAR':43,'Gas':486,'GAS_FIRED_STEAM':486, 'COMBINED_CYCLE':486,
+                  'Gas cogen':486,'Biomass':52, 'ENERGY STORAGE':33, 'COAL':1001,
+                  'DUAL FUEL':743.5, 'Oil/Gas':633,'Dual Fuel':743.5}
+#The .map function simply fills out the 'Emissions' column based on 
+# k,v pairs in 'emission_factors' + a little extra math I tacked on.
+    dft['Emissions'] = dft['Sub Fuel Type'].map(emission_factors) * dft['Volume'] / 1000
+    
+    
+    diff = datetime.now() - start_time
+    print(f'After mapping: {diff}')
+    
+    df_temp = dft.copy()
+    df_hourly_vol = pd.DataFrame()
+
+    #Selecting rows of df that match each Fuel Type and then summing their volume and ghgs
+    for i in dft['Fuel Type'].unique():
+        #print(i) #for debugging
+        df_temp = dft.loc[(dft['Fuel Type'] == i)]
+        df_hourly_vol[i]= df_temp.groupby('Date (MST)')['Volume'].sum()
+        df_hourly_vol[f'{i}_ghg'] = df_temp.groupby('Date (MST)')['Emissions'].sum()
+    df_hourly_vol = df_hourly_vol.reset_index()
+    
+    #making total volume column
+    df_hourly_vol['Total Load'] = df_hourly_vol[['OTHER', 'WIND', 'GAS', 'HYDRO', 'SOLAR',
+           'ENERGY STORAGE', 'COAL', 'DUAL FUEL']].sum(axis=1)
+    #making hourly total ghg column
+    df_hourly_vol['Total_ghg'] = df_hourly_vol[['OTHER_ghg','WIND_ghg','GAS_ghg','HYDRO_ghg',
+                                                'SOLAR_ghg','ENERGY STORAGE_ghg','COAL_ghg',
+                                                'DUAL FUEL_ghg']].sum(axis=1)
+    
+    #Calculating Emission Intensity (ie gCO2e/kWh)
+    df_hourly_vol['EI'] = (df_hourly_vol['Total_ghg'] / df_hourly_vol['Total Load']) * 1000
+    #Making columns for renewable volume and FF volume
+    df_hourly_vol['Renewable Gen'] = df_hourly_vol[['WIND','SOLAR','HYDRO']].sum(axis=1)
+    df_hourly_vol['Fossil Fuel Gen'] = df_hourly_vol[['OTHER','GAS','COAL','DUAL FUEL']].sum(axis=1)
+    df_hourly_vol['Net Load'] = df_hourly_vol['Total Load'] - (df_hourly_vol['Renewable Gen'])
+    
+    
+    diff = datetime.now() - start_time
+    print(f'After EI math: {diff}')
+    
+    
+    # Getting price data
+    file = 'P&A Table_data.csv'
+    
+    price_data=pd.read_csv(file)
+    
+    df_price = price_data
+    df_price = df_price.set_index('Date - MST')
+    df_price2 = df_price.pivot(columns='Measure Names',values='Measure Values')
+    df_price2 = df_price2.reset_index()
+    df_price2 = df_price2.rename(columns = {'Date - MST': 'Date (MST)'})
+    df_price2['Date (MST)'] = pd.to_datetime(df_price2['Date (MST)'],format='%m/%d/%Y %I:%M:%S %p')
+    df_price2 = df_price2.reset_index(drop=True)
+    df_price2 = df_price2[['Date (MST)','Avg. Price']]
+    df_price2 = df_price2.loc[(df_price2['Date (MST)'].dt.date >= start_date) & (df_price2['Date (MST)'].dt.date<= end_date)]
+    
+    #Merging Generation hourly dataset with the price dataset
+    
+    df_hourly_vol = pd.merge(df_hourly_vol,df_price2,how='left',on='Date (MST)')
+    df_hourly_vol['RE_percent'] = ((df_hourly_vol['Renewable Gen'] / df_hourly_vol['Total Load']) *100).round(decimals = 1)
+
+
+    diff = datetime.now() - start_time
+    print(f'After price math: {diff}')
+    return dfg, df_hourly_vol
+#%%
+def price_ei_vol_week(start_date='2024-01-02',end_date=start_date):
+    start_time = datetime.now()
+    dft = df.copy()
+    diff = datetime.now() - start_time
+    print(f'Copying df: {diff}')
+    dft['Date (MST)'] = pd.to_datetime(df['Date (MST)'],format='%Y-%m-%d %H:%M:%S')
+    
+    
+    # Restricting df to desired dates, in app this value comes from the callback
+
+    start_date = pd.to_datetime(start_date).date() #Format: %Y/%m/%d
+    end_date = pd.to_datetime(end_date).date()  #Same format
+
+    # You have to use dt.date for series and .date() for individual strings 
+    dft = dft.loc[(dft['Date (MST)'].dt.date >= start_date) & (dft['Date (MST)'].dt.date<= end_date)]
+
+    dfg = dft.groupby(['Date (MST)','Fuel Type'])['Volume'].sum()
+    dfg = dfg.reset_index()
+    
+    diff = datetime.now() - start_time
+    print(f'Filtering df & making dfg: {diff}')
+    
+#Emission factor calculations from: https://www.nrel.gov/docs/fy21osti/80580.pdf
+# Values in gCO2e/kWh, conversion explained in next cell.
+
+#This method is used in favour of the cell above because it's more succinct and faster computationally
+
+    """"Volumes from AESO are in MW and emission factors (above) are in 
+    gCO2e/kWh. This means we have to multiply values by 1x10^3 but divide by 1x10^6
+    to get tonsCO2e. This means dividing all values by 1x10^3"""
+
+    dft['Emissions'] = np.nan
+
+    emission_factors={'Wood/Refuse':52,'WIND':13, 'SIMPLE_CYCLE':486,'COGENERATION':486,
+                  'HYDRO':24,'SOLAR':43,'Gas':486,'GAS_FIRED_STEAM':486, 'COMBINED_CYCLE':486,
+                  'Gas cogen':486,'Biomass':52, 'ENERGY STORAGE':33, 'COAL':1001,
+                  'DUAL FUEL':743.5, 'Oil/Gas':633,'Dual Fuel':743.5}
+#The .map function simply fills out the 'Emissions' column based on 
+# k,v pairs in 'emission_factors' + a little extra math I tacked on.
+    diff = datetime.now() - start_time
+    print(f'After mapping: {diff}')
+
+    dft['Emissions'] = dft['Sub Fuel Type'].map(emission_factors) * dft['Volume'] / 1000
+
+    df_temp = dft.copy()
+    df_hourly_vol = pd.DataFrame()
+
+    #Selecting rows of df that match each Fuel Type and then summing their volume and ghgs
+    for i in dft['Fuel Type'].unique():
+        df_temp = dft.loc[(dft['Fuel Type'] == i)]
+        df_hourly_vol[i]= df_temp.groupby('Date (MST)')['Volume'].sum()
+        df_hourly_vol[f'{i}_ghg'] = df_temp.groupby('Date (MST)')['Emissions'].sum()
+    df_hourly_vol = df_hourly_vol.reset_index()
+    
+    #making total volume column
+    df_hourly_vol['Total Load'] = df_hourly_vol[['OTHER', 'WIND', 'GAS', 'HYDRO', 'SOLAR','ENERGY STORAGE', 'COAL', 'DUAL FUEL']].sum(axis=1)
+    #making hourly total ghg column
+    df_hourly_vol['Total_ghg'] = df_hourly_vol[['OTHER_ghg','WIND_ghg','GAS_ghg','HYDRO_ghg',
+                                                'SOLAR_ghg','ENERGY STORAGE_ghg','COAL_ghg',
+                                                'DUAL FUEL_ghg']].sum(axis=1)
+    
+    #Calculating Emission Intensity (ie gCO2e/kWh)
+    df_hourly_vol['EI'] = (df_hourly_vol['Total_ghg'] / df_hourly_vol['Total Load']) * 1000
+    #Making columns for renewable volume and FF volume
+    df_hourly_vol['Renewable Gen'] = df_hourly_vol[['WIND','SOLAR','HYDRO']].sum(axis=1)
+    df_hourly_vol['Fossil Fuel Gen'] = df_hourly_vol[['OTHER','GAS','COAL','DUAL FUEL']].sum(axis=1)
+    df_hourly_vol['Net Load'] = df_hourly_vol['Total Load'] - (df_hourly_vol['Renewable Gen'])
+    
+    diff = datetime.now() - start_time
+    print(f'After EI math: {diff}')
+    # Getting price data
+    file = 'P&A Table_data.csv'
+    
+    price_data=pd.read_csv(file)
+    
+    df_price = price_data
+    df_price = df_price.set_index('Date - MST')
+    df_price2 = df_price.pivot(columns='Measure Names',values='Measure Values')
+    df_price2 = df_price2.reset_index()
+    df_price2 = df_price2.rename(columns = {'Date - MST': 'Date (MST)'})
+    df_price2['Date (MST)'] = pd.to_datetime(df_price2['Date (MST)'],format='%m/%d/%Y %I:%M:%S %p')
+    df_price2 = df_price2.reset_index(drop=True)
+    df_price2 = df_price2[['Date (MST)','Avg. Price']]
+    df_price2 = df_price2.loc[(df_price2['Date (MST)'].dt.date >= start_date) & (df_price2['Date (MST)'].dt.date<= end_date)]
+    
+    #Merging Generation hourly dataset with the price dataset
+    
+    df_hourly_vol = pd.merge(df_hourly_vol,df_price2,how='left',on='Date (MST)')
+    df_hourly_vol['RE_percent'] = ((df_hourly_vol['Renewable Gen'] / df_hourly_vol['Total Load']) *100).round(decimals = 1)
+    diff = datetime.now() - start_time
+    print(f'After incorporating price data: {diff}')
+    
+    return df_hourly_vol, dfg
+
---
+++ app_new.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 13:38:59 2024
+
+@author: aaronkirkey
+"""
+
+#%% Environment setup
+import pandas as pd
+import numpy as np
+import matplotlib as mpl 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sp
+import glob
+import sys
+from matplotlib import cm
+import pathlib
+import os
+from sklearn import preprocessing
+import plotly.io as pio
+import plotly.express as px
+from dash import Dash, html, dash_table, dcc, callback, Input, Output
+import dash
+import dash_bootstrap_components as dbc
+import app_data_new
+import datetime as dt
+from datetime import date
+from datetime import datetime
+pio.renderers.default='browser'
+
+#%%  Dash stuff
+# datte = '2024-01-06' # Placeholder value
+app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.SANDSTONE])
+app.title = 'AESO Energy Dash'
+app.layout = html.Div([
+    html.Div([html.H1(children='AESO Energy Dash',style={'textAlign': 'center', 'fontSize':48}),
+    html.Br(),
+    html.Div(children=
+             """*Disclaimer: This web-dash and contents are in no way 
+             affiliated with the AESO and serves only as a personal project to 
+             improve my data science and data viz skillset with respect to grid
+             data. For questions, comments or improvement ideas, please contact
+             me at: akirkey2@gmail.com with the subject line 'AESO dash project'.
+             Thank you for your interest!""")]),
+     html.Br(),
+     html.Br(),
+     html.Br(),
+
+    html.Div([
+        
+        dcc.Tabs([
+            dcc.Tab(children=[ 
+                 html.Br(),
+                 html.Br(),
+                 html.Div(children='Choose Date: ',style={'fontSize': 20}),
+                 dcc.DatePickerSingle(  # This is a drop-down calendar that updates the elements on the page
+                         id='date-picker-single',
+                         min_date_allowed=date(2024, 1, 1),
+                         max_date_allowed=date(2024, 6, 30),
+                         initial_visible_month=date(2024, 1, 1),
+                         date=date(2024, 1, 1),
+                         display_format='Y-M-D'),
+                 html.Br(),
+                 html.Br(),
+                 html.Div(children = 'Date chosen: (for debugging)*',style={'fontSize': 20}), # Visual feedback on page displaying date
+                 dcc.Markdown(id='date_display',children = ''),
+                 html.Br(),
+                 # html.Div(children ='Hourly Data',style={'fontSize': 32,'font-weight':'bold','textAlign':'center'}),
+                 html.Br(),
+                 html.Br(),
+                 #This Div is a graph with title, left adjusted beside the following Div
+                 html.Div(children=[
+                     html.Div(children='Hourly generation on AESO by asset & fuel type',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     html.Div(dcc.Graph(id='asset_gen'),
+                                      style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'})
+                                     ],
+                                     style={'display':'inline-block','width': '70%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 
+                html.Div(children=[
+                    html.Div(children='Generation by source',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),    
+                    html.Div(dcc.Graph(id='gen_pie_daily'),style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'}),
+                                ],
+                                style={'display':'inline-block','width': '30%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 
+                 #This Div is a graph with title, right adjusted beside the previous Div
+                 html.Div(children=[
+                     html.Div(children='% Generation from Renewable vs. Fossil Fuel sources',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     html.Div(dcc.Graph(id='clean_fossil_gen'),
+                              style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'})
+                                     ],
+                                     style={'display':'inline-block','width': '50%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 
+                 #This Div is a graph with title, left adjusted beside the following Div
+                 html.Div(children=[
+                     html.Div(children='Hourly Marginal Price',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     html.Div(dcc.Graph(id='gen_price'),
+                                      style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'})
+                                     ],
+                                     style={'display':'inline-block','width': '50%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 
+                 #This Div is a graph with title, right adjusted beside the previous Div
+                 html.Div(children=[
+                     html.Div(children='Hourly Energy Storage Charge/Discharge',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     html.Div(dcc.Graph(id='storage_volume'),
+                              style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'})
+                                     ],
+                                     style={'display':'inline-block','width': '50%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 html.Div(children=[
+                     html.Div(children='Emission Intensity of Generated Electricity (tonsCO2e/MWh)',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     html.Div(dcc.Graph(id='EI_graph'),
+                              style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'})
+                                     ],
+                                     style={'display':'inline-block','width': '50%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 
+                 
+                 #Reference Tables
+                 html.Div(children ='Hourly Statistics',style={'fontSize': 32,'font-weight':'bold','textAlign':'center'}),
+                 html.Br(),
+                 
+                 # This Div contains a table and a title and is left adjusted beside following Div
+                 html.Div(children=[
+                     html.Div(children='Contribution on the grid (MW)',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     dash_table.DataTable(
+                                         id='summary_table', 
+                                         page_size=24,
+                                         css=[{'selector': 'table', 'rule': 'width: 800px; height: 300px; margin: 0 auto; overflow: auto;'},
+                                              {'selector': 'td', 'rule': 'font-size: 12px;'},  # Adjusting font size for table cells
+                                              {'selector': 'th', 'rule': 'font-size: 12px; font-weight: bold;'}
+                                              ]
+                                         )
+                                     ],
+                                     style={'display':'inline-block','width': '49%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+             
+             
+                 # This Div contains a table and a title and is right adjusted beside previous Div
+                 html.Div(children=[
+                     html.Div(children='% Contribution on the grid',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                     dash_table.DataTable(
+                                         id='percent_table',
+                                         page_size=24,
+                                         css=[{'selector': 'table', 'rule': 'width: 800px; height: 300px; margin: 0 auto; overflow: auto;'},
+                                              {'selector': 'td', 'rule': 'font-size: 12px;'},  # Adjusting font size for table cells
+                                              {'selector': 'th', 'rule': 'font-size: 12px; font-weight: bold;'}
+                                              ]
+                                         )
+                                     ],
+                                     style={'display':'inline-block','width': '49%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+                 ],
+                 label='Single Day'),
+        dcc.Tab(children=[
+            html.Br(),
+            dcc.DatePickerRange(
+                id='my-date-picker-range',
+                min_date_allowed=date(2024, 1, 1),
+                max_date_allowed=date(2024, 6, 30),
+                initial_visible_month=date(2024, 1, 1),
+                start_date=date(2024, 1, 1),
+                end_date=date(2024, 1, 4)
+                                ),
+            html.Br(),
+            html.Div(children = 'Dates chosen: (for debugging)*',style={'fontSize': 20}), # Visual feedback on page displaying date
+            dcc.Markdown(id='date_range_display',children = ''),
+            html.Br(),
+            
+            html.Div(children=[
+                html.Div(children='Generation by asset type',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),
+                html.Div(dcc.Graph(id='asset_gen_multi'),
+                     style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'}),
+            ],
+                style={'display':'inline-block','width': '70%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+            
+            
+           html.Div(children=[
+               html.Div(children='% generation by asset type',style={'fontSize': 22,'font-weight':'bold','textAlign':'center'}),    
+               html.Div(dcc.Graph(id='gen_pie_multi'),style={'display': 'inline-block', 'width': '100%', 'textAlign': 'center'}),
+                           ],
+                           style={'display':'inline-block','width': '30%', 'verticalAlign': 'top', 'textAlign': 'center'}),
+            
+                    ],label='Multi-Day'),
+                ]),#Closing dcc.Tabs
+                html.Br(),
+                          ]),
+        ]) #this closes out the app layout
+
+
+
+#Callback for the daily tab
+@callback(
+    Output(component_id='date_display',component_property ='children'),
+    Output('summary_table', component_property='data'),
+    Output('percent_table','data'),
+    Output('asset_gen', 'figure'),
+    Output('clean_fossil_gen','figure'),
+    Output('gen_price', 'figure'),
+    Output('storage_volume','figure'),
+    Output('EI_graph','figure'),
+    Output('gen_pie_daily','figure'),
+             
+    Input(component_id='date-picker-single', component_property ='date'))
+
+
+#Calls functions with updated date to make dash reactive to selected date
+def update_date(user_selected):    
+    print(user_selected) #The function argument comes from the component property of the Input
+    df_daily = app_data_new.price_ei_vol_day(user_selected)
+    
+    summary_data = app_data_new.df_hourly(user_selected).to_dict('records')
+    percent_data = app_data_new.df_percent(user_selected).to_dict('records')
+    asset_gen_fig = px.area(df_daily[0], x='Date (MST)', y="Volume", color="Fuel Type",labels={"y": "Generation Volume MWh"})
+    asset_gen_fig.update_layout(yaxis_title='Generation Volume (MW)')
+    clean_ff_fig = px.line(df_daily[1],x='Date (MST)',y=['Total Load','Net Load'],color_discrete_sequence=['black', 'gray'],range_y=[0,None],labels={"y": "Generation Volume MWh"})
+    clean_ff_fig.update_layout(yaxis_title='Generation Volume (MW)')
+    price_fig = px.line(df_daily[1],x='Date (MST)',y='Avg. Price')
+    price_fig.update_layout(yaxis_title='Average Hourly Price ($/MW)')
+    storage_fig = px.area(app_data_new.df_hourly(user_selected),x='Date (MST)',y='ENERGY STORAGE')
+    storage_fig.update_layout(yaxis_title='Energy Storage Volume (MW)')
+    EI_fig = px.scatter(df_daily[1],x='Date (MST)',y='RE_percent',color = 'EI', color_continuous_scale=['green', 'yellow', '#D62728'])
+    EI_fig.update_layout(yaxis_title='% Generation from Renewables')
+    gen_pie_daily = px.pie(df_daily[0], values='Volume', names='Fuel Type') #title='% Generation over timeframe')
+    return user_selected, summary_data, percent_data, asset_gen_fig, clean_ff_fig, price_fig, storage_fig, EI_fig, gen_pie_daily # The returned object is assigned to the component property of the Output
+
+@callback( #WORKING HERE TO CHANGE THE OUTPUT TEXT. REFER TO EXAMPLE ON PLOTLY FORUM
+    Output('date_range_display','children'),
+    Output('asset_gen_multi','figure'),
+    Output('gen_pie_multi','figure'),
+    # Output('output-container-date-picker-range', 'children'),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date'))
+def update_output(start_date, end_date):
+    renge = f"{start_date}   -->   {end_date}"
+    print(renge)
+    if start_date <= end_date:
+        df_multi = app_data_new.price_ei_vol_week(start_date,end_date)
+        gen_multi = px.area(df_multi[1], x='Date (MST)', y="Volume", color="Fuel Type",labels={"y": "Generation Volume MWh"})
+        gen_pie_multi = px.pie(df_multi[1], values='Volume', names='Fuel Type')#, title='% Generation over timeframe')
+    else:
+        gen_multi = 'LOADING...'
+        gen_pie_multi = 'LOADING...'
+        # multi_gen = px.area(app_data_new.price_ei_vol_week(start_date,end_date=start_date)[1], x='Date (MST)', y="Volume", color="Fuel Type",labels={"y": "Generation Volume MWh"})
+        # gen_pie_multi = px.pie(app_data_new.price_ei_vol_week(start_date,end_date=start_date)[1], values='Volume', names='Fuel Type', title='% Generation over timeframe')
+    return renge, gen_multi, gen_pie_multi
+    
+# @callback(
+#     Output('asset_gen_multi','figure'),   
+#     Input('my-date-picker-range', 'start_date'),
+#     Input('my-date-picker-range', 'end_date')
+#     )
+# def update_multi(start_date,end_date):
+#     gen_fig_multi = px.area(app_data_new.price_ei_vol_day(start_date,end_date), x='Date (MST)', y="Volume", color="Fuel Type",labels={"y": "Generation Volume MWh"})
+#     return gen_fig_multi
+
+#The following code is for running locally in a browser
+if __name__ == '__main__':
+    app.run_server(port=2222)
+
+#This is for running the dash on Ploomber.io
+# server = app.server
+    
